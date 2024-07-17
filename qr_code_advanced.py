b577bc33476f9749d75c10be9cf98e8f06b393e3
eliasdengo <eliasdengo5@gmail.com>
2024-07-17 10:40:41 +0300

    change incontent

--- qr_code_advanced.py
+++ qr_code_advanced.py
@@ -1,6 +1,21 @@
-import segno 
-from urllib.request import urlopen 
-dengos_qrcode=segno.make_qr("https://www.youtube.com/watch?v=tcxDCRSm-0A") 
-eliasdengo_url=urlopen("http://eliasdengo.github.io/") 
+import segno
 
-dengos_qrcode.to_artistic(background=eliasdengo_url,target="elias_dengo.gif",scale=5)
+# Define the URL to encode
+url = "https://www.youtube.com/watch?v=tcxDCRSm-0A"
+
+# Create the QR code object
+dengos_qrcode = segno.make_qr(url)
+
+# Download Elias Dengo's website content (optional)
+# This might not be necessary depending on your artistic goal
+
+# elias_dengo_url = urlopen("http://eliasdengo.github.io/")  # Commented out
+
+# Create artistic QR code with a white background (optional)
+# This avoids downloading external content, which can be unreliable
+white_background = (255, 255, 255)  # RGB values for white
+dengos_qrcode.artistic(background_color=white_background)
+
+# Save the QR code as a GIF image
+dengos_qrcode.save("elias_dengo.gif", scale=5)
+
