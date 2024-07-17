b78ff5e007e37a8cb8ff628941b341287797bc23
eliasdengo <eliasdengo5@gmail.com>
2024-07-17 10:03:55 +0300

    complate practice with qrcode module

--- qr_code12.py
+++ qr_code12.py
@@ -1,3 +1,7 @@
 import  qrcode 
 from PIL import Image
-qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10,border=4,)
+qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10,border=4,) 
+qr.add_data("https://eliasdengo.github.io/")
+qr.make(fit=True) 
+img=qr.make_image(fill_color="red",back_color="blue") 
+img.save("eliasdengo.png")
