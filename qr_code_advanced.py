4370ed56b8fe768a46d7573b990ab16f84151cd1
eliasdengo <eliasdengo5@gmail.com>
2024-07-17 10:37:48 +0300

    advanced qr code generatin by gif for more advertisment and artistic work

---
+++ qr_code_advanced.py
@@ -0,0 +1,6 @@
+import segno 
+from urllib.request import urlopen 
+dengos_qrcode=segno.make_qr("https://www.youtube.com/watch?v=tcxDCRSm-0A") 
+eliasdengo_url=urlopen("http://eliasdengo.github.io/") 
+
+dengos_qrcode.to_artistic(background=eliasdengo_url,target="elias_dengo.gif",scale=5,)
