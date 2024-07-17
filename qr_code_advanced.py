a4aa6f9e38e3e42b2e9e928027a59e14233f5f16
eliasdengo <eliasdengo5@gmail.com>
2024-07-17 10:39:22 +0300

    some change in leter

--- qr_code_advanced.py
+++ qr_code_advanced.py
@@ -3,4 +3,4 @@ from urllib.request import urlopen
 dengos_qrcode=segno.make_qr("https://www.youtube.com/watch?v=tcxDCRSm-0A") 
 eliasdengo_url=urlopen("http://eliasdengo.github.io/") 
 
-dengos_qrcode.to_artistic(background=eliasdengo_url,target="elias_dengo.gif",scale=5,)
+dengos_qrcode.to_artistic(background=eliasdengo_url,target="elias_dengo.gif",scale=5)
